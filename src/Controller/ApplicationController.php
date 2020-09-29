765d91d3095af2908264bc624b51cb9678de5964
tesdy14 <tesdy14@hotmail.fr>
2020-09-29 16:07:33 +0200

    ajout de webpack

---
+++ src/Controller/ApplicationController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApplicationController extends AbstractController
+{
+    /**
+     * @Route("/", name="application")
+     */
+    public function index()
+    {
+
+        return $this->render('application/index.html.twig', [
+            'controller_name' => 'ApplicationController',
+        ]);
+    }
+}
